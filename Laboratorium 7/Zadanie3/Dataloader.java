4446d4c4e335b47a8eac2cb8920117916c89fdbd
AleksanderOstasz <48862309+AleksanderOstasz@users.noreply.github.com>
2019-06-18 16:10:15 +0200

    Add files via upload

---
+++ Laboratorium 7/Zadanie3/Dataloader.java
@@ -0,0 +1,9 @@
+﻿/*
+Autor: Aleksander Ostasz
+Zadanie 3  Lab 7
+ */
+
+public interface Dataloader
+{
+    public void loadData(Cache cache);
+}
---
+++ Laboratorium 7/Zadanie3/Repeater.java
@@ -0,0 +1,24 @@
+﻿/*
+Autor: Aleksander Ostasz
+Zadanie 3  Lab 7 
+ */
+
+import java.util.TimerTask;
+
+class Repeater extends TimerTask 
+{
+    Cache cache;
+    public Repeater(Cache cache) 
+    {
+        this.cache = cache;
+    }
+
+    public void run()
+    {
+        CsvLoader csvLoader = new CsvLoader();
+        cache.resetCache();
+        csvLoader.loadData(cache);
+
+        cache.printCache();
+    }
+}
