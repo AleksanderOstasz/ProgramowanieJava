4337f1995610b357afa391f05ab2681b0a26e3a3
AleksanderOstasz <48862309+AleksanderOstasz@users.noreply.github.com>
2019-06-18 11:37:53 +0200

    Add files via upload

---
+++ Laboratorium 6/Zadanie 4/Runner.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercise4;
+
+/**
+ *
+ * @author Maciej Falkiewicz, Aleksander Ostasz
+ */
+public interface Runner {
+
+    void run();
+}
